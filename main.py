2d6004d42e714af42824822df756493c622a7738
Stefan Bie <bie.stefan@systemverification.com>
2019-06-11 15:43:13 +0200

    TAaS#3 Perfromance test fw
    thread, scenario and user class

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
-from selenium import webdriver
-import logging
-import threading
-import time
+from threading import Thread
 from test_selenium import *
+import time
 
+class thread(Thread):
+
+    def __init__(self, user, scenario):
+        Thread.__init__(self)
+        self.user = user
+        self.scenario = scenario
+        self.daemon = True
+        self.stop_user = False
+        #self.options = webdriver.ChromeOptions()
+        #self.options.headless = True
+        self.driver = webdriver.Chrome()
+        self.start()
+
+    def stop(self):
+        self.stop_user = True
+
+    def run(self):
+        while(True):
+            if(self.stop_user):
+                self.driver.quit()
+                break
+            self.scenario.method(self.driver)
+            time.sleep(1)
+
+
+class User():
+    def __init__(self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+
+class Scenario():
+    def __init__(self, id, method, probability):
+        self.id = id
+        self.method = method
+        self.probability = probability
+
+scenario_pool = []
+user_pool = []
+
+def get_scenario():
+    return scenario_pool[0]
+
+def get_user():
+    return user_pool[0]
 
 if __name__ == "__main__":
+    users = 3
+    test_time = 10
+    scenario_pool.append(Scenario(id=1, method=test_sundsvall, probability=1))
+    scenario_pool.append(Scenario(id=2, method=test_arboga, probability=3))
+    user_pool.append(User(id=1, name="gunnar", password="gunnar"))
+    user_pool.append(User(id=2, name="arne", password="arne"))
+    scenario = get_scenario()
+    user = get_user()
 
+    t = thread(user=user, scenario=scenario)
 
-    options = webdriver.ChromeOptions()
-    options.binary_location = r'C:\Users\stefanb\AppData\Local\Google\Chrome SxS\Application\chrome.exe'
-    #options.headless = True
-    #options.add_argument('window-size=1200x600')
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=test_sundsvall, args=(webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Python\chromedriver77.exe', options=options),))
-    y = threading.Thread(target=test_arboga, args=(webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Python\chromedriver77.exe', options=options),))
-    logging.info("Main    : before running thread")
-    x.start()
-    time.sleep(2)
-    y.start()
-    logging.info("Main    : wait for the thread to finish")
-    #x.join()
-    logging.info("Main    : all done")
+    time.sleep(test_time)
+    t.stop()
+    t.join()
